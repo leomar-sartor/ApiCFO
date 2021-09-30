ec835ccd1d73b4a61e7ed608ee47833788266def
leomar.sartor <leomar.sartor@funcionalcorp.com.br>
2021-09-30 15:44:53 -0300

    Add project files.

---
+++ CrawlerCFO/Controllers/CfoController.cs
@@ -0,0 +1,69 @@
+﻿using AngleSharp;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrawlerCFO.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CfoController : ControllerBase
+    {
+        private string _url = $"https://website.cfo.org.br/profissionais-cadastrados/?cro=ESTADO&inscricao=CREDENCIAL";
+        private Dentista _dentista;
+
+        [HttpGet("{estado}/{credencial}")]
+        public async Task<IActionResult> consultaDadoset(string estado, string credencial)
+        {
+            _url = _url.Replace("ESTADO", estado).Replace("CREDENCIAL", credencial);
+
+            var context = BrowsingContext.New(
+               Configuration.Default.WithDefaultLoader()
+               .WithDefaultCookies()
+               );
+
+            var Page = await context.OpenAsync(_url);
+
+            var Results = Page.GetElementsByClassName("entry-content").FirstOrDefault();
+            Results.QuerySelector("div").Remove();
+            Results.QuerySelector("script").Remove();
+            Results.QuerySelector("h6").Remove();
+
+            var nosComValor = Results.ChildNodes
+                .Where(n => !String.IsNullOrWhiteSpace(n.TextContent) && n.NodeName != "HR" && n.NodeName != "A")
+                .ToList();
+
+            if (nosComValor.Count > 6)
+            {
+                nosComValor.RemoveAt(0);
+                nosComValor.RemoveAt(nosComValor.Count - 1);
+                nosComValor.RemoveAt(nosComValor.Count - 1);
+            }
+
+            if (nosComValor.Count == 6)
+            {
+                try
+                {
+                    _dentista = new Dentista(
+                        nosComValor[0].TextContent.Split("-")[0],
+                        nosComValor[0].TextContent.Split("-")[1].Split(":")[1],
+                        nosComValor[1].TextContent,
+                        nosComValor[2].TextContent.Split(":")[1],
+                        nosComValor[3].TextContent.Split(":")[1],
+                        nosComValor[4].TextContent.Split(":")[1].Trim(),
+                        nosComValor[5].TextContent.Split(":")[1].Trim()
+                    );
+
+                    return Ok(_dentista);
+                }
+                catch (Exception e)
+                {
+                    return BadRequest(e);
+                }
+            }
+
+            return NotFound("CFO não encontrado!");
+        }
+    }
+}
---
+++ CrawlerCFO/Dentista.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CrawlerCFO
+{
+    public class Dentista
+    {
+        public Dentista(string funcao, string inscricao, string nome, string situacao, string tipo, string inscricaoCRO, string registroCRO)
+        {
+            Funcao = funcao;
+            Inscricao = inscricao;
+            Nome = nome;
+            Situacao = situacao;
+            Tipo = tipo;
+
+            var diaInsc = Int32.Parse(inscricaoCRO.Split("/")[0]);
+            var mesInsc = Int32.Parse(inscricaoCRO.Split("/")[1]);
+            var anoInsc = Int32.Parse(inscricaoCRO.Split("/")[2]);
+            InscricaoCRO = new DateTime(anoInsc, mesInsc, diaInsc);
+
+            var diaReg = Int32.Parse(registroCRO.Split("/")[0]);
+            var mesReg = Int32.Parse(registroCRO.Split("/")[1]);
+            var anoReg = Int32.Parse(registroCRO.Split("/")[2]);
+            RegistroCRO = new DateTime(anoReg, mesReg, diaReg);
+        }
+
+        public string Funcao { get; set; }
+        public string Inscricao { get; set; }
+        public string Situacao { get; set; }
+        public string Tipo { get; set; }
+        public string Nome { get; set; }
+        public DateTime InscricaoCRO { get; set; }
+        public DateTime RegistroCRO { get; set; }
+    }
+}
